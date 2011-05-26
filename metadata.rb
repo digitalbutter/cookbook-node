1588ece168a14e12426953bde0ef36f3986f0e1a
Emmanuel Prochasson <eprochasson@gmail.com>
2011-05-26 11:51:01 +0800

    new release, better upstart, see issue https://github.com/digitalbutter/cookbook-node/pull/3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "devops@butter.com.hk"
 license          "Apache 2.0"
 description      "Installs/Configures node, npm and node server providers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+version          "0.0.4"
 depends          "git"
 supports         "ubuntu"
 recipe           "node-js", "Install node and npm"
