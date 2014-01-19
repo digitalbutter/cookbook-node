2fb652a6d20849d5cbb0b4a0d406a2c128b63196
pseudomuto <david.muto@gmail.com>
2014-01-19 11:10:47 -0500

    allow specifying global npm packages

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default[:node][:revision]      = "HEAD"
 default[:node][:repo_url]      = "https://github.com/joyent/node.git"
 default[:node][:user]          = "nodejs"
+default[:node][:packages]      = []
