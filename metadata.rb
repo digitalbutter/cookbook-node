530732e5b833c865e5a20a1f6916bc73b3d059b0
Ed Bosher <edbosher@gmail.com>
2014-03-18 17:30:32 +0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "devops@butter.com.hk"
 license          "Apache 2.0"
 description      "Installs/Configures node, npm and node server providers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.6"
+version          "1.1.2"
 depends          "git"
 depends          "runit"
 supports         "ubuntu"
