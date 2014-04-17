dfad36d872294b4f7287e3b711cb99575b729e32
Ed Bosher <edbosher@gmail.com>
2014-04-17 13:34:36 +0800

    bumping version number and adding dependency on build-essential

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,10 @@ maintainer_email "devops@butter.com.hk"
 license          "Apache 2.0"
 description      "Installs/Configures node, npm and node server providers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.1.2"
+version          "1.1.3"
 depends          "git"
 depends          "runit"
+depends          "build-essential"
 supports         "ubuntu"
 recipe           "node-js", "Install node and npm"
 
